e1995325e1ef53712403a6455f1398d353faec85
Will Boyd <shazzenbot@gmail.com>
2019-09-26 11:59:52 -0400

    tweak starting state

--- src/main/java/com/linuxacademy/ccdak/consumer/MemberSignupsConsumer.java
+++ src/main/java/com/linuxacademy/ccdak/consumer/MemberSignupsConsumer.java
@@ -24,6 +24,8 @@ public class MemberSignupsConsumer {
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         
+        props.setProperty("auto.offset.reset", "none");
+        
         consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList("member_signups"));
     }
